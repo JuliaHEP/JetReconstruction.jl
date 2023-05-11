3e0f7c3b16200bc4ba6f37996e556625cdf6bc49
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2023-05-11 17:42:42 -0400

    Better version of chep.jl wrapper code
    Add profiling and flamegraph code
    Reformat with JuliaFormatter
    This version used for CHEP2023 presentation

--- chep.jl
+++ chep.jl
@@ -1,18 +1,24 @@
 #! /usr/bin/env julia
 """
-Wrapper to run Atell's jet reco code feeding in the standard set of HepMC events that
+Wrapper to run jet reco code feeding in the standard set of HepMC events that
 are used for benchmarking jet reco.
 """
 
 import HepMC3
+import FlameGraphs
+import ProfileSVG
+
 using ArgParse
+using Profile
+using Colors
+using StatProfilerHTML
 
 using JetReconstruction
 
 const R = 0.4
 const ptmin = 5.0
 
-read_events(fname, maxevents=-1, skipevents = 0) = begin
+function read_events(fname; maxevents=-1, skipevents=0)
     f = open(fname)
 
     events = Vector{PseudoJet}[]
@@ -22,7 +28,10 @@ read_events(fname, maxevents=-1, skipevents = 0) = begin
         input_particles = PseudoJet[]
         for p in parts
             if p.status == 1
-                push!(input_particles, PseudoJet(p.momentum.px, p.momentum.py, p.momentum.pz, p.momentum.e))
+                push!(
+                    input_particles,
+                    PseudoJet(p.momentum.px, p.momentum.py, p.momentum.pz, p.momentum.e),
+                )
             end
         end
         push!(events, input_particles)
@@ -33,20 +42,25 @@ read_events(fname, maxevents=-1, skipevents = 0) = begin
     events
 end
 
-pseudojets2vectors(events::Vector{Vector{PseudoJet}}) = begin
+function pseudojets2vectors(events::Vector{Vector{PseudoJet}})
     """Convert events in PseudoJets into deep vectors"""
     event_vector = Vector{Vector{Vector{Float64}}}(undef, size(events)[1])
     for (ievent, event) in enumerate(events)
         jet_struct = Vector{Vector{Float64}}(undef, size(event)[1])
         for (ipart, initial_particle) in enumerate(event)
-            jet_struct[ipart] = [initial_particle.px, initial_particle.py, initial_particle.pz, initial_particle.E]
+            jet_struct[ipart] = [
+                initial_particle.px,
+                initial_particle.py,
+                initial_particle.pz,
+                initial_particle.E,
+            ]
         end
         event_vector[ievent] = jet_struct
     end
     event_vector
 end
 
-final_jets(jets::Vector{Vector{Float64}}, ptmin::AbstractFloat) = begin
+function final_jets(jets::Vector{Vector{Float64}}, ptmin::AbstractFloat)
     count = 0
     final_jets = Vector{FinalJet}()
     sizehint!(final_jets, 6)
@@ -60,8 +74,52 @@ final_jets(jets::Vector{Vector{Float64}}, ptmin::AbstractFloat) = begin
     end
     final_jets
 end
- 
-in_mem_process(events::Vector{Vector{PseudoJet}}, nsamples::Integer=1, dump::Bool=false, gcoff::Bool=false) = begin
+
+function profile_code(events, niters)
+    Profile.init(n=10^6, delay=0.00001)
+    profile_events(events) = begin
+        for evt in events
+            anti_kt_algo(evt, R=0.4)
+        end
+    end
+    profile_events(events[1:1])
+    @profile for i = 1:niters
+        profile_events(events)
+    end
+    statprofilehtml()
+    fcolor = FlameGraphs.FlameColors(
+        reverse(colormap("Blues", 15))[1:5],
+        colorant"slategray4",
+        colorant"gray95",
+        reverse(colormap("Reds", 15))[1:5],
+        reverse(sequential_palette(39, 10; s=38, b=2))[1:5],#brownish pallette
+    )
+    ProfileSVG.save(
+        fcolor,
+        joinpath("statprof", "profsvg.svg");
+        combine=true,
+        timeunit=:ms,
+        font="Arial, Helvetica, sans-serif"
+    )
+    println(
+        "Flame graph from ProfileSVG.jl at file://",
+        abspath("statprof/profsvg.svg"),
+        "\n",
+        """
+\tRed tint:          Runtime dispatch
+\tBrown/yellow tint: Garbage collection
+\tBlue tint:         OK
+""",
+    )
+end
+
+function jet_process(
+    events::Vector{Vector{PseudoJet}};
+    nsamples::Integer=1,
+    gcoff::Bool=false,
+    profile::Bool=false,
+    dump::Union{String,Nothing}=nothing
+)
     println("Will process $(size(events)) events")
 
     # First, convert all events into the Vector of Vectors that Atell's
@@ -69,10 +127,12 @@ in_mem_process(events::Vector{Vector{PseudoJet}}, nsamples::Integer=1, dump::Boo
     event_vector = pseudojets2vectors(events)
 
     # If we are dumping the results, setup the JSON structure
-    if dump jet_collection = FinalJets[] end
+    if !isnothing(dump)
+        jet_collection = FinalJets[]
+    end
 
     # Warmup code if we are doing a multi-sample timing run
-    if nsamples > 1
+    if nsamples > 1 || profile
         println("Doing initial warm-up run")
         for event in event_vector
             anti_kt_algo(event, R=0.4)
@@ -82,19 +142,30 @@ in_mem_process(events::Vector{Vector{PseudoJet}}, nsamples::Integer=1, dump::Boo
     GC.gc()
     gcoff && GC.enable(false)
 
+    if profile
+        profile_code(event_vector, nsamples)
+        return Nothing
+    end
+
     # Now setup timers and run the loop
     cummulative_time = 0.0
     cummulative_time2 = 0.0
-    for irun in 1:nsamples
+    for irun = 1:nsamples
         print("$(irun)/$(nsamples) ")
         t_start = time_ns()
         for (ievt, event) in enumerate(event_vector)
-            finaljets, finalind = anti_kt_algo(event, R=0.4)
+            finaljets, _ = anti_kt_algo(event, R=0.4)
             fj = final_jets(finaljets, ptmin)
-            if dump && irun==1 push!(jet_collection, FinalJets(ievt, fj)) end
+            if !isnothing(dump) && irun == 1
+                println("Event $(ievt)")
+                for (ijet, jet) in enumerate(fj)
+                    println(" $(ijet) - $(jet)")
+                end
+                push!(jet_collection, FinalJets(ievt, fj))
+            end
         end
         t_stop = time_ns()
-        dt_μs = convert(Float64, t_stop-t_start) * 1.e-3
+        dt_μs = convert(Float64, t_stop - t_start) * 1.e-3
         println(dt_μs)
         cummulative_time += dt_μs
         cummulative_time2 += dt_μs^2
@@ -104,8 +175,8 @@ in_mem_process(events::Vector{Vector{PseudoJet}}, nsamples::Integer=1, dump::Boo
 
     mean = cummulative_time / nsamples
     cummulative_time2 /= nsamples
-    if nsamples>1
-        sigma = sqrt(nsamples/(nsamples-1) * (cummulative_time2 - mean^2))
+    if nsamples > 1
+        sigma = sqrt(nsamples / (nsamples - 1) * (cummulative_time2 - mean^2))
     else
         sigma = 0.0
     end
@@ -114,8 +185,8 @@ in_mem_process(events::Vector{Vector{PseudoJet}}, nsamples::Integer=1, dump::Boo
     println("Processed $(length(events)) events $(nsamples) times")
     println("Time per event $(mean) ± $(sigma) μs")
 
-    if dump
-        open("jet_collections_julia_basic.json", "w") do io
+    if !isnothing(dump)
+        open(dump, "w") do io
             JSON3.pretty(io, jet_collection)
         end
     end
@@ -123,49 +194,46 @@ end
 
 parse_command_line(args) = begin
     s = ArgParseSettings(autofix_names=true)
-    @add_arg_table s begin
+    @add_arg_table! s begin
         "--maxevents", "-n"
         help = "Maximum number of events to read. -1 to read all events from the  file."
         arg_type = Int
         default = -1
+
         "--skip", "-s"
         help = "Number of events to skip at beginning of the file."
         arg_type = Int
         default = 0
+
         "--nsamples", "-m"
         help = "Number of measurement points to acquire."
         arg_type = Int
         default = 1
+
         "--gcoff"
         help = "Turn off Julia garbage collector during each time measurement."
         action = :store_true
+
         "--profile"
         help = "Profile code and generate a flame graph."
         action = :store_true
-        "--profile-only"
-        help = "Profile code and generate a flame graph. Skip the code timing."
-        action = :store_true
-        "--alloc"
-        help = "Provide memory allocation statistics."
-        action = :store_true
+
         "--dump"
-        help = "Display list of recontructed jets"
-        action = :store_true
-        "--json"
-        help = "Write list of recontructed jets to a JSON file"
-        action = :store_true
+        help = "Write list of recontructed jets to a JSON formatted file and also to stdout"
+
         "file"
         help = "HepMC3 event file in HepMC3 to read."
         required = true
     end
-    return parse_args(args, s; as_symbols = true)
+    return parse_args(args, s; as_symbols=true)
 end
 
 main() = begin
     args = parse_command_line(ARGS)
-    events::Vector{Vector{PseudoJet}} = read_events(args[:file], args[:maxevents], args[:skip])
-    in_mem_process(events, args[:nsamples], args[:dump],
-                    args[:gcoff])
+    events::Vector{Vector{PseudoJet}} =
+        read_events(args[:file], maxevents=args[:maxevents], skipevents=args[:skip])
+    jet_process(events, nsamples=args[:nsamples], gcoff=args[:gcoff], profile=args[:profile],
+        dump=args[:dump])
     nothing
 end
 
