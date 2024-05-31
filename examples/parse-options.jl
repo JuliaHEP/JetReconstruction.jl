cc205e16e14743485bf37ffdc6c916c3e9a97dec
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-05-31 17:19:39 +0200

    Basic jet visualisation example
    Read an event from a HepMC3 file, reconstruct and then visualise it
    (save to file)
    
    Fixes to JetViz.jl to allow it to be used more easily from example
    scripts by specifying JetReconstruction module and using namespace

---
+++ examples/parse-options.jl
@@ -0,0 +1,20 @@
+"""
+Add `parse_item` code for interpeting `JetAlgorithm.Algorithm` and `RecoStrategy.Strategy` types
+from the command line.
+"""
+
+function ArgParse.parse_item(::Type{RecoStrategy.Strategy}, x::AbstractString)
+	s = tryparse(RecoStrategy.Strategy, x)
+	if s === nothing
+		throw(ErrorException("Invalid value for strategy: $(x)"))
+	end
+	s
+end
+
+function ArgParse.parse_item(::Type{JetAlgorithm.Algorithm}, x::AbstractString)
+	s = tryparse(JetAlgorithm.Algorithm, x)
+	if s === nothing
+		throw(ErrorException("Invalid value for algorithm: $(x)"))
+	end
+	s
+end
---
+++ examples/visualise-jets.jl
@@ -0,0 +1,70 @@
+#! /usr/bin/env julia
+"""Use the visualisation tools to plot the reconstructed jets"""
+
+using ArgParse
+using Logging
+using CairoMakie
+
+using JetReconstruction
+
+include(joinpath(@__DIR__, "parse-options.jl"))
+include(joinpath(dirname(pathof(JetReconstruction)), "jetvis.jl"))
+
+function main()
+
+    s = ArgParseSettings(autofix_names = true)
+    @add_arg_table s begin
+        "--event", "-e"
+        help = "Event in file to visualise"
+        arg_type = Int
+        default = 1
+
+        "--ptmin"
+        help = "Minimum p_t for final inclusive jets (energy unit is the same as the input clusters, usually GeV)"
+        arg_type = Float64
+        default = 5.0
+
+        "--exclusive-dcut"
+        help = "Return all exclusive jets where further merging would have d>d_cut"
+        arg_type = Float64
+
+        "--exclusive-njets"
+        help = "Return all exclusive jets once clusterisation has produced n jets"
+        arg_type = Int
+
+        "--distance", "-R"
+        help = "Distance parameter for jet merging"
+        arg_type = Float64
+        default = 0.4
+
+        "--algorithm", "-A"
+        help = """Algorithm to use for jet reconstruction: $(join(JetReconstruction.AllJetRecoAlgorithms, ", "))"""
+        arg_type = JetAlgorithm.Algorithm
+        default = JetAlgorithm.AntiKt
+
+        "--strategy", "-S"
+        help = """Strategy for the algorithm, valid values: $(join(JetReconstruction.AllJetRecoStrategies, ", "))"""
+        arg_type = RecoStrategy.Strategy
+        default = RecoStrategy.Best
+
+        "file"
+        help = "HepMC3 event file in HepMC3 to read"
+        required = true
+    end
+    args = parse_args(ARGS, s; as_symbols = true)
+
+
+    logger = ConsoleLogger(stdout, Logging.Info)
+    global_logger(logger)
+
+    events::Vector{Vector{PseudoJet}} =
+        read_final_state_particles(args[:file], maxevents = 1, skipevents = args[:event] - 1)
+
+    power = JetReconstruction.algorithm2power[args[:algorithm]]
+    cs = jet_reconstruct(events[1], R = args[:distance], p = power, strategy = args[:strategy])
+
+    plt = jetsplot(events[1], cs; Module = CairoMakie)
+    save("jetvis.png", plt)
+end
+
+main()
