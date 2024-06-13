0cb9cb8bfb39f5c4d0b21179994b78ffe7bfefe9
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-06-13 16:27:05 +0200

    Documentation deployment (#58)
    * Documentation deployment
    
    * Fix formatting

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,6 @@ using Documenter
 using JetReconstruction
 
 makedocs(sitename = "JetReconstruction.jl")
+
+deploydocs(repo = "github.com/JuliaHEP/JetReconstruction.jl.git",
+           push_preview = true)
