c3e09e502b848b4c7db1810448c8fe8838cc0387
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-06-13 16:24:59 +0200

    Fix formatting

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,5 @@ using JetReconstruction
 
 makedocs(sitename = "JetReconstruction.jl")
 
-deploydocs(
-    repo = "github.com/JuliaHEP/JetReconstruction.jl.git",
-    push_preview = true
-)
+deploydocs(repo = "github.com/JuliaHEP/JetReconstruction.jl.git",
+           push_preview = true)
