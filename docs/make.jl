a788bcbe981aae83d94e7dac0ed8ead043a7e0ea
Moelf <proton@jling.dev>
2024-07-11 15:01:28 +0200

    use DocumenterVitepress.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter
+using DocumenterVitepress
 using CairoMakie
 using JetReconstruction
 
@@ -6,14 +7,17 @@ push!(LOAD_PATH, "../ext/")
 
 include(joinpath(@__DIR__, "..", "ext", "JetVisualisation.jl"))
 
-makedocs(sitename = "JetReconstruction.jl",
-         pages = [
-             "Home" => "index.md",
-             "Examples" => "examples.md",
-             "Reference" => Any["Public API" => "lib/public.md",
-                                "Internal API" => "lib/internal.md",
-                                "Visualisation API" => "lib/visualisation.md"]
-         ])
+makedocs(sitename="JetReconstruction.jl",
+    format=MarkdownVitepress(
+        repo="github.com/JuliaHEP/JetReconstruction.jl",
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Examples" => "examples.md",
+        "Reference" => Any["Public API"=>"lib/public.md",
+            "Internal API"=>"lib/internal.md",
+            "Visualisation API"=>"lib/visualisation.md"]
+    ])
 
 deploydocs(repo = "github.com/JuliaHEP/JetReconstruction.jl.git",
            push_preview = true)
