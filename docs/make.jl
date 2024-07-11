ac6f52d1086c96db9f1666f7919a34eb24686575
Moelf <proton@jling.dev>
2024-07-11 23:19:40 +0200

    format

--- docs/make.jl
+++ docs/make.jl
@@ -7,17 +7,15 @@ push!(LOAD_PATH, "../ext/")
 
 include(joinpath(@__DIR__, "..", "ext", "JetVisualisation.jl"))
 
-makedocs(sitename="JetReconstruction.jl",
-    format=MarkdownVitepress(
-        repo="github.com/JuliaHEP/JetReconstruction.jl",
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Examples" => "examples.md",
-        "Reference" => Any["Public API"=>"lib/public.md",
-            "Internal API"=>"lib/internal.md",
-            "Visualisation API"=>"lib/visualisation.md"]
-    ])
+makedocs(sitename = "JetReconstruction.jl",
+         format = MarkdownVitepress(repo = "github.com/JuliaHEP/JetReconstruction.jl"),
+         pages = [
+             "Home" => "index.md",
+             "Examples" => "examples.md",
+             "Reference" => Any["Public API" => "lib/public.md",
+                                "Internal API" => "lib/internal.md",
+                                "Visualisation API" => "lib/visualisation.md"]
+         ])
 
 deploydocs(repo = "github.com/JuliaHEP/JetReconstruction.jl.git",
            push_preview = true)
