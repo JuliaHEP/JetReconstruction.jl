9f22634a651806e2359dab38e40cbd1153253207
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-29 17:13:19 +0200

    Format

--- test/tests_examples.jl
+++ test/tests_examples.jl
@@ -11,21 +11,21 @@ run(`julia --project=$example_dir $test_dir/bootstrap_examples.jl`)
 # Now run a few tests with our examples
 @testset "Jet Reconstruction Examples" begin
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/jetreco.jl -S N2Plain -A AntiKt -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/jetreco.jl -S N2Tiled -p 1 -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/jetreco.jl -p 1.5 -A GenKt -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
 
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/instrumented-jetreco.jl -S N2Plain -A AntiKt -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/instrumented-jetreco.jl -S N2Tiled -p 1 -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/instrumented-jetreco.jl -p 1.5 -A GenKt -R 1.0 $events_file`,
-                                devnull)))
+                               devnull)))
 
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/jetreco-constituents.jl`,
-                                devnull)))
+                               devnull)))
     @test success(run(pipeline(`julia --project=$example_dir $example_dir/visualise-jets.jl -A AntiKt --event 5 -R 2.0 $events_file $example_dir/jetvis.png`,
-                                devnull)))
+                               devnull)))
 end
