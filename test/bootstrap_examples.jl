ffdbca4a55811d3e4c5e9814fb2e90c974569e4e
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-29 18:14:45 +0200

    More sophisticated examples setup script

--- test/bootstrap_examples.jl
+++ test/bootstrap_examples.jl
@@ -3,10 +3,23 @@
 
 println("Starting example boostrap script")
 
+if isfile(joinpath(@__DIR__, "..", "examples", "Manifest.toml"))
+    println("Exisiting Manifest.toml found - assuming environment is already setup")
+    exit(0)
+end
+
+if occursin("JetReconstruction.jl", pwd())
+    local_path = joinpath("..", "JetReconstruction.jl")
+else
+    local_path = joinpath("..", "JetReconstruction")
+end
+
+println("Trying local path: $local_path (from $(pwd()))")
+
 using Pkg
 Pkg.instantiate()
 Pkg.resolve()
-Pkg.develop(path = "../JetReconstruction")
+Pkg.develop(path = local_path)
 Pkg.status()
 
 println("Finished example boostrap script")
