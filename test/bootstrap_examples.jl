930cfa86e2bae6dfbc07fd927d00a88fb78811ab
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-29 17:46:42 +0200

    More example bootstrap debugging

--- test/bootstrap_examples.jl
+++ test/bootstrap_examples.jl
@@ -1,6 +1,12 @@
 # Bootstrap script to setup this local version of the JetReconstruction package
 # for running the examples
 
+println("Starting example boostrap script")
+
 using Pkg
 Pkg.instantiate()
+Pkg.resolve()
 Pkg.develop(path = "../JetReconstruction")
+Pkg.status()
+
+println("Finished example boostrap script")
