6f887351faa41b05abb917945db166af111ddf97
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-30 09:22:26 +0200

    Brain fart

--- test/bootstrap_examples.jl
+++ test/bootstrap_examples.jl
@@ -8,13 +8,13 @@ if isfile(joinpath(@__DIR__, "..", "examples", "Manifest.toml"))
     exit(0)
 end
 
-$local_pkg_path = realpath(joinpath(@__DIR__, "..", "..", "JetReconstruction"))
+local_pkg_path = realpath(joinpath(@__DIR__, "..", "..", "JetReconstruction"))
 
-if !isdir($local_pkg_path)
+if !isdir(local_pkg_path)
     # Try a symlink to the current checkout
-    $local_checkout_path = realpath(joinpath(@__DIR__, "..", ".."))
+    local_checkout_path = realpath(joinpath(@__DIR__, "..", ".."))
     println("Creating symlink from $local_pkg_path to $local_checkout_path")
-    symlink($local_checkout_path, $local_pkg_path)
+    symlink($local_checkout_path, local_pkg_path)
 end
 
 println("Setting up examples project")
@@ -23,7 +23,7 @@ using Pkg
 Pkg.instantiate()
 Pkg.resolve()
 println(("Seting JetReconstruction development package path: $local_pkg_path"))
-Pkg.develop(path = $local_pkg_path)
+Pkg.develop(path = local_pkg_path)
 Pkg.status()
 
 println("Finished examples boostrap script")
