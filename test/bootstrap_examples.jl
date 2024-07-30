282746d68ff2c91bd7eb15a68a1ce289581653ab
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-07-30 10:02:59 +0200

    More debugging

--- test/bootstrap_examples.jl
+++ test/bootstrap_examples.jl
@@ -3,18 +3,23 @@
 
 println("Starting example boostrap script")
 
-if isfile(joinpath(@__DIR__, "..", "examples", "Manifest.toml"))
-    println("Exisiting Manifest.toml found - assuming environment is already setup")
-    exit(0)
-end
+# if isfile(joinpath(@__DIR__, "..", "examples", "Manifest.toml"))
+#     println("Exisiting Manifest.toml found - assuming environment is already setup")
+#     exit(0)
+# end
 
 local_pkg_path = joinpath(@__DIR__, "..", "..", "JetReconstruction")
+
+
 
 if !isdir(local_pkg_path)
     # Try a symlink to the current checkout
     local_checkout_path = realpath(joinpath(@__DIR__, "..", ".."))
     println("Creating symlink from $local_pkg_path to $local_checkout_path")
     symlink(local_checkout_path, local_pkg_path)
+else
+    println(realpath(local_pkg_path))
+    println(readdir(local_pkg_path))
 end
 
 println("Setting up examples project")
