0fe16a44ffb286c1536ba665d0d7fb985ca85e02
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-08-02 11:02:05 +0200

    Add test of Durham algorithm on ee->H events

---
+++ test/test-ee-reconstruction.jl
@@ -0,0 +1,10 @@
+# Tests of e+e- reconstruction algorithms
+
+include("common.jl")
+
+durham_njets3 = ComparisonTest(events_file_ee,
+                               joinpath(@__DIR__, "data", "jet-collections-fastjet-njets3-Durham-eeH.json.gz"),
+                               JetAlgorithm.Durham, RecoStrategy.N2Plain, 1, 1.0,
+                               (cs) -> exclusive_jets(cs; njets = 3))
+                               
+run_reco_test(durham_njets3)
