02141bad6c0c5cccf1f73127a00b2b1259d1ecd9
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-08-30 17:28:53 +0200

    Add tests for EEKt
    Which are currently broken!

--- test/test-ee-reconstruction.jl
+++ test/test-ee-reconstruction.jl
@@ -9,3 +9,11 @@ durham_njets3 = ComparisonTest(events_file_ee,
                                (cs) -> exclusive_jets(cs; njets = 3), "exclusive njets")
 
 run_reco_test(durham_njets3)
+
+eekt_inclusive = ComparisonTest(events_file_ee,
+                               joinpath(@__DIR__, "data",
+                                        "jet-collections-fastjet-inclusive-EEKt-p-1-R1.0.json.gz"),
+                               JetAlgorithm.EEKt, RecoStrategy.N2Plain, -1, 1.0,
+                               (cs) -> inclusive_jets(cs; ptmin = 5.0), "inclusive")
+
+run_reco_test(eekt_inclusive)
