1b245bade1e996576c3d70524ecf24679ae503bb
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-10-24 15:14:20 +0200

    Small improvement to EDM4hep test script

--- examples/EDM4hep/EDM4hepJets.jl
+++ examples/EDM4hep/EDM4hepJets.jl
@@ -12,6 +12,15 @@ function parse_command_line(args)
         arg_type = Int
         default = 1
 
+        "--printjets"
+        help = "Print reconstructed jets (selecting njets from the reconstructed event)"
+        action = :store_true
+
+        "--njets"
+        help = "Number of jets to output"
+        arg_type = Int
+        default = 2
+
         "file"
         help = "EDM4hep events file to read"
         required = true
@@ -25,25 +34,39 @@ function main()
     global_logger(logger)
 
     # Open input event file
+    global_timer_start = time_ns()
     reader = RootIO.Reader(args[:file])
     events = RootIO.get(reader, "events")
 
+    total_time = 0.0
+    count = 0
+
     # Reconstruct each event
     for (ievt, evt) in enumerate(events)
+        count += 1
         if args[:maxevents] > 0 && ievt > args[:maxevents]
             break
         end
         recps = RootIO.get(reader, evt, "ReconstructedParticles")
 
+        start_jet = time_ns()
         cs = jet_reconstruct(recps; algorithm = JetAlgorithm.Durham)
-        @info begin
-            jets = "Event $(ievt)\n"
-            for jet in exclusive_jets(cs; njets = 2, T = EEjet)
-                jets *= " $jet\n"
+        end_jet = time_ns()
+        total_time += end_jet - start_jet
+        if args[:printjets]
+            @info begin
+                jets = "Event $(ievt)\n"
+                for jet in exclusive_jets(cs; njets = args[:njets], T = EEjet)
+                    jets *= " $jet\n"
+                end
+                jets
             end
-            jets
         end
     end
+    global_timer_stop = time_ns()
+    @info "Reconstructed $(count) events in $(total_time/1.0e9)s: rate = $((count * 1.0e9) / total_time)Hz"
+    @info "Total time: $((global_timer_stop - global_timer_start)/1.0e9)s"
+    count, total_time
 end
 
 main()
