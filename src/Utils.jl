1e20f7bcfaa4c962d86e62f8e30f55d23aebe112
Moelf <proton@jling.dev>
2024-10-28 00:20:39 +0100

    format

--- src/Utils.jl
+++ src/Utils.jl
@@ -133,7 +133,7 @@ array.
 - `dij_min`: The minimum value in the first `n` elements of the `dij` array.
 - `best`: The index of the minimum value in the `dij` array.
 """
-function fast_findmin(dij::DenseVector{T}, n) where T
+function fast_findmin(dij::DenseVector{T}, n) where {T}
     laneIndices = SIMD.Vec{8, Int}((1, 2, 3, 4, 5, 6, 7, 8))
     minvals = SIMD.Vec{8, T}(Inf)
     min_indices = SIMD.Vec{8, Int}(0)
@@ -152,15 +152,18 @@ function fast_findmin(dij::DenseVector{T}, n) where T
     end
 
     min_value = SIMD.minimum(minvals)
-    min_index = @inbounds min_value == minvals[1] ? min_indices[1] : min_value == minvals[2] ? min_indices[2] :
-                min_value == minvals[3] ? min_indices[3] : min_value == minvals[4] ? min_indices[4] :
-                min_value == minvals[5] ? min_indices[5] : min_value == minvals[6] ? min_indices[6] :
-                min_value == minvals[7] ? min_indices[7] : min_indices[8]
+    min_index = @inbounds min_value == minvals[1] ? min_indices[1] :
+                          min_value == minvals[2] ? min_indices[2] :
+                          min_value == minvals[3] ? min_indices[3] :
+                          min_value == minvals[4] ? min_indices[4] :
+                          min_value == minvals[5] ? min_indices[5] :
+                          min_value == minvals[6] ? min_indices[6] :
+                          min_value == minvals[7] ? min_indices[7] : min_indices[8]
 
     @inbounds @fastmath for _ in 1:remainder
         xi = dij[i]
         pred = dij[i] < min_value
-        min_value= ifelse(pred, xi, min_value)
+        min_value = ifelse(pred, xi, min_value)
         min_index = ifelse(pred, i, min_index)
         i += 1
     end
