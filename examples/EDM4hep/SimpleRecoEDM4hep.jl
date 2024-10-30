115e9971e56f5234cc5ea14065546c486ea76636
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-10-30 09:03:23 +0100

    Add constituent retrieval to basic example

--- examples/EDM4hep/SimpleRecoEDM4hep.jl
+++ examples/EDM4hep/SimpleRecoEDM4hep.jl
@@ -6,13 +6,21 @@ using JetReconstruction
 input_file = joinpath("/", "Users", "graemes", "code", "EDM4hepJets", "data",
                       "events_196755633.root")
 reader = RootIO.Reader(input_file)
-events = RootIO.get(reader, "events")
+events = RootIO.get(reader, "events");
 
-evt = events[1]
+evt = events[1];
 
 recps = RootIO.get(reader, evt, "ReconstructedParticles")
 
+# Reconstruct and print the jets
 cs = jet_reconstruct(recps; algorithm = JetAlgorithm.Durham)
-for jet in exclusive_jets(cs; njets = 2, T = EEjet)
+dijets = exclusive_jets(cs; njets = 2, T = EEjet)
+for jet in dijets
     println(jet)
+end
+
+# Get constituents
+for (i, jet) in enumerate(dijets)
+    my_constituent_indexes = constituent_indexes(jet, cs)
+    println("Jet $i constituents: $my_constituent_indexes")
 end
