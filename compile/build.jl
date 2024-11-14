037c816d84530cf45c4e46b642b374525928ff03
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-11-14 17:27:39 +0100

    add precompile execution, remove precompile statements, use args for build script

--- compile/build.jl
+++ compile/build.jl
@@ -1,16 +1,31 @@
 using PackageCompiler
+using ArgParse
 
-output_directory = joinpath(splitdir(@__DIR__) |> first, "JetReconstructionCompiled")
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--source-dir"
+    help = "Directory containing the source files"
+    arg_type = String
+    default = splitdir(@__DIR__) |> first
 
-@info "Creating library in $output_directory"
-PackageCompiler.create_library(".", output_directory;
+    "--output-dir"
+    help = "Directory to save the compiled library"
+    arg_type = String
+    default = joinpath(splitdir(@__DIR__) |> first, "JetReconstructionCompiled")
+end
+
+parsed_args = parse_args(s)
+source_dir = parsed_args["source-dir"]
+output_dir = parsed_args["output-dir"]
+
+@info "Compiling package from $source_dir"
+@info "Creating library in $output_dir"
+PackageCompiler.create_library(source_dir, output_dir;
                                lib_name = "jetreconstruction",
                                header_files = [joinpath(@__DIR__, "include",
                                                         "JetReconstruction.h")],
-                               #       precompile_execution_file = [joinpath(@__DIR__,
-                               #                                             "precompile_execution.jl")],
-                               #       precompile_statements_file = [jointpath(@__DIR__,
-                               #                                               "precompile_statements.jl")],
+                               precompile_execution_file = [joinpath(@__DIR__,
+                                                                     "precompile_execution.jl")],
                                incremental = false,
                                filter_stdlibs = true,
                                force = true)
