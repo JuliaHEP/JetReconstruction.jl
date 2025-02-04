a904f6c0bf1d8b5f7885d68c9f2cfe7757c458bc
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-02-04 08:05:40 +0100

    move test parameters to constants

--- test/test-pp-reconstruction.jl
+++ test/test-pp-reconstruction.jl
@@ -3,12 +3,15 @@
 # Contains all common functions and necessary imports
 include("common.jl")
 
+const test_genkt_power = 1.5
+const test_cone_size = 0.4
+
 # Test inclusive jets for each algorithm and strategy
 for alg in [JetAlgorithm.AntiKt, JetAlgorithm.CA, JetAlgorithm.Kt, JetAlgorithm.GenKt],
     stg in [RecoStrategy.N2Plain, RecoStrategy.N2Tiled]
 
     if alg == JetAlgorithm.GenKt
-        power = 1.5
+        power = test_genkt_power
         fastjet_file = joinpath(@__DIR__, "data",
                                 "jet-collections-fastjet-inclusive-$(alg)-p$(power).json.gz")
     else
@@ -19,7 +22,7 @@ for alg in [JetAlgorithm.AntiKt, JetAlgorithm.CA, JetAlgorithm.Kt, JetAlgorithm.
 
     test = ComparisonTest(events_file_pp, fastjet_file,
                           alg, stg,
-                          power, 0.4,
+                          power, test_cone_size,
                           (cs) -> inclusive_jets(cs; ptmin = 5.0), "inclusive")
     run_reco_test(test)
 end
@@ -30,7 +33,7 @@ for alg in [JetAlgorithm.CA, JetAlgorithm.Kt]
                             "jet-collections-fastjet-njets4-$(alg).json.gz")
     test = ComparisonTest(events_file_pp, fastjet_file,
                           alg, RecoStrategy.Best,
-                          JetReconstruction.algorithm2power[alg], 0.4,
+                          JetReconstruction.algorithm2power[alg], test_cone_size,
                           (cs) -> exclusive_jets(cs; njets = 4), "exclusive njets")
     run_reco_test(test)
 end
@@ -41,7 +44,7 @@ for (alg, dij_max) in zip([JetAlgorithm.CA, JetAlgorithm.Kt], ["0.99", "20.0"])
                             "jet-collections-fastjet-dij$(dij_max)-$(alg).json.gz")
     test = ComparisonTest(events_file_pp, fastjet_file,
                           alg, RecoStrategy.Best,
-                          JetReconstruction.algorithm2power[alg], 0.4,
+                          JetReconstruction.algorithm2power[alg], test_cone_size,
                           (cs) -> exclusive_jets(cs; dcut = tryparse(Float64, dij_max)),
                           "exclusive dcut")
     run_reco_test(test)
