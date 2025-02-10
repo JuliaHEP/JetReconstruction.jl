bdbb5a8accd3661effcb9ca2857c92248d1409ee
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-02-10 14:53:48 +0100

    cover more scenarios in precompile

--- compile/precompile_execution.jl
+++ compile/precompile_execution.jl
@@ -1,7 +1,7 @@
 # Dummy code to call and cache compilation of all C-bindings
-# The numeric values doesn't make sense and are used only to select correct dispatch
+# The numeric values don't make sense and are used only to select correct dispatch
 
-using JetReconstruction
+using JetReconstruction: PseudoJet, RecoStrategy, JetAlgorithm
 using JetReconstruction.C_JetReconstruction: jetreconstruction_PseudoJet_init,
                                              C_ClusterSequence,
                                              jetreconstruction_ClusterSequence_free_members_,
@@ -10,34 +10,52 @@ using JetReconstruction.C_JetReconstruction: jetreconstruction_PseudoJet_init,
                                              jetreconstruction_jet_reconstruct,
                                              jetreconstruction_inclusive_jets,
                                              jetreconstruction_exclusive_jets_njets,
-                                             jetreconstruction_exclusive_jets_dcut
+                                             jetreconstruction_exclusive_jets_dcut,
+                                             StatusCode
 
-pseudoJets_len = Csize_t(2)
-pseudoJets_ptr = Ptr{PseudoJet}(Libc.malloc(pseudoJets_len * sizeof(PseudoJet)))
+function assert_ok(ret_val)
+    @assert StatusCode.T(ret_val) == StatusCode.OK
+end
 
-jetreconstruction_PseudoJet_init(pseudoJets_ptr, 0.0, 1.0, 2.0, 3.0)
-jetreconstruction_PseudoJet_init(pseudoJets_ptr + sizeof(PseudoJet), 1.0, 2.0, 3.0, 4.0)
-
-strategy = RecoStrategy.Best
-algorithm = JetAlgorithm.CA
 R = 2.0
+jets_len = Csize_t(2)
 
+### pp jets ###
+
+jets_ptr = Ptr{PseudoJet}(Libc.malloc(jets_len * sizeof(PseudoJet)))
+@assert jets_ptr != C_NULL
 clustersequence_ptr = Ptr{C_ClusterSequence{PseudoJet}}(Libc.malloc(sizeof(C_ClusterSequence{PseudoJet})))
-jetreconstruction_jet_reconstruct(pseudoJets_ptr, pseudoJets_len, algorithm, R,
-                                  RecoStrategy.Best,
-                                  clustersequence_ptr)
-
+@assert clustersequence_ptr != C_NULL
 results_ptr = Ptr{C_JetsResult{PseudoJet}}(Libc.malloc(sizeof(C_JetsResult{PseudoJet})))
-jetreconstruction_exclusive_jets_njets(clustersequence_ptr, Csize_t(2), results_ptr)
-jetreconstruction_JetsResult_free_members_(results_ptr)
+@assert results_ptr != C_NULL
 
-jetreconstruction_exclusive_jets_dcut(clustersequence_ptr, 1.0, results_ptr)
-jetreconstruction_JetsResult_free_members_(results_ptr)
+assert_ok(jetreconstruction_PseudoJet_init(jets_ptr, 0.0, 1.0, 2.0, 3.0))
+assert_ok(jetreconstruction_PseudoJet_init(jets_ptr + sizeof(PseudoJet), 1.0, 2.0, 3.0,
+                                           4.0))
 
-jetreconstruction_inclusive_jets(clustersequence_ptr, 0.0, results_ptr)
-jetreconstruction_JetsResult_free_members_(results_ptr)
+for algorithm in [JetAlgorithm.AntiKt, JetAlgorithm.CA, JetAlgorithm.Kt]
+    for strategy in [RecoStrategy.N2Plain, RecoStrategy.N2Tiled, RecoStrategy.Best]
+        assert_ok(jetreconstruction_jet_reconstruct(jets_ptr, jets_len, algorithm, R,
+                                                    strategy,
+                                                    clustersequence_ptr))
 
-jetreconstruction_ClusterSequence_free_members_(clustersequence_ptr)
-Libc.free(pseudoJets_ptr)
+        assert_ok(jetreconstruction_inclusive_jets(clustersequence_ptr, 0.0, results_ptr))
+        jetreconstruction_JetsResult_free_members_(results_ptr)
+
+        if algorithm != JetAlgorithm.AntiKt
+            assert_ok(jetreconstruction_exclusive_jets_njets(clustersequence_ptr,
+                                                             Csize_t(2),
+                                                             results_ptr))
+            jetreconstruction_JetsResult_free_members_(results_ptr)
+
+            assert_ok(jetreconstruction_exclusive_jets_dcut(clustersequence_ptr, 1.0,
+                                                            results_ptr))
+            jetreconstruction_JetsResult_free_members_(results_ptr)
+
+            jetreconstruction_ClusterSequence_free_members_(clustersequence_ptr)
+        end
+    end
+end
+Libc.free(jets_ptr)
 Libc.free(clustersequence_ptr)
 Libc.free(results_ptr)
