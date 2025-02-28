c1adad1f9bc3c46bbb7f0310b344383f6418e0db
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2025-02-28 10:21:13 +0100

    Formatting fixes

--- test/test-compare-types.jl
+++ test/test-compare-types.jl
@@ -3,11 +3,11 @@
 include("common.jl")
 
 function do_test_compare_types(strategy::RecoStrategy.Strategy;
-    algname = "Unknown",
-    ptmin::Float64 = 5.0,
-    distance::Float64 = 0.4,
-    power::Integer = -1)
-    
+                               algname = "Unknown",
+                               ptmin::Float64 = 5.0,
+                               distance::Float64 = 0.4,
+                               power::Integer = -1)
+
     # Strategy
     if (strategy == RecoStrategy.N2Plain)
         jet_reconstruction = plain_jet_reconstruct
@@ -18,29 +18,29 @@ function do_test_compare_types(strategy::RecoStrategy.Strategy;
     else
         throw(ErrorException("Strategy not yet implemented"))
     end
-    
+
     # Now run our jet reconstruction...
     # From PseudoJets
     events::Vector{Vector{PseudoJet}} = read_final_state_particles(events_file_pp)
     jet_collection = FinalJets[]
     for (ievt, event) in enumerate(events)
         finaljets = final_jets(inclusive_jets(jet_reconstruction(event, R = distance,
-        p = power), ptmin = ptmin))
+                                                                 p = power), ptmin = ptmin))
         sort_jets!(finaljets)
         push!(jet_collection, FinalJets(ievt, finaljets))
     end
-    
+
     # From LorentzVector
     events_lv::Vector{Vector{LorentzVector}} = read_final_state_particles(events_file_pp;
-    T = LorentzVector)
+                                                                          T = LorentzVector)
     jet_collection_lv = FinalJets[]
     for (ievt, event) in enumerate(events_lv)
         finaljets = final_jets(inclusive_jets(jet_reconstruction(event, R = distance,
-        p = power), ptmin = ptmin))
+                                                                 p = power), ptmin = ptmin))
         sort_jets!(finaljets)
         push!(jet_collection_lv, FinalJets(ievt, finaljets))
     end
-    
+
     @testset "Jet Reconstruction Compare PseudoJet and LorentzVector, Strategy $strategy_name, Algorithm $algname" begin
         # Here we test that inputting LorentzVector gave the same results as PseudoJets
         for (ievt, (event, event_lv)) in enumerate(zip(jet_collection, jet_collection_lv))
@@ -59,4 +59,3 @@ end
 
 do_test_compare_types(RecoStrategy.N2Plain, algname = pp_algorithms[-1], power = -1)
 do_test_compare_types(RecoStrategy.N2Tiled, algname = pp_algorithms[-1], power = -1)
-
--- test/test-jet_reconstruct-if.jl
+++ test/test-jet_reconstruct-if.jl
@@ -5,25 +5,43 @@ include("common.jl")
 let inputs = JetReconstruction.read_final_state_particles(events_file_ee)
     @testset "jet_reconstruct() interface" begin
         # EE Algorithms
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Durham)) == ClusterSequence{EEjet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.EEKt, p=-1, R=1.0)) == ClusterSequence{EEjet}
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.EEKt)) == ClusterSequence{EEjet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Durham)) ==
+              ClusterSequence{EEjet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.EEKt, p = -1,
+                                     R = 1.0)) == ClusterSequence{EEjet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1];
+                                                          algorithm = JetAlgorithm.EEKt))==ClusterSequence{EEjet}
 
         # PP Algorithms
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.AntiKt)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.CA)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Kt)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.AntiKt, p=-1)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.CA, p=0)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Kt, p=1)) == ClusterSequence{PseudoJet}
-        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.GenKt, p=1.0, R=0.4)) == ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.AntiKt)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.CA)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Kt)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.AntiKt, p = -1)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.CA, p = 0)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Kt, p = 1)) ==
+              ClusterSequence{PseudoJet}
+        @test typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.GenKt, p = 1.0,
+                                     R = 0.4)) == ClusterSequence{PseudoJet}
 
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.AntiKt, p=0)) == ClusterSequence{PseudoJet}
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.CA, p=1)) == ClusterSequence{PseudoJet}
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.Kt, p=-1)) == ClusterSequence{PseudoJet}
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]; algorithm = JetAlgorithm.GenKt, R=0.4)) == ClusterSequence{PseudoJet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1];
+                                                          algorithm = JetAlgorithm.AntiKt,
+                                                          p = 0))==ClusterSequence{PseudoJet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1];
+                                                          algorithm = JetAlgorithm.CA,
+                                                          p = 1))==ClusterSequence{PseudoJet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1];
+                                                          algorithm = JetAlgorithm.Kt,
+                                                          p = -1))==ClusterSequence{PseudoJet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1];
+                                                          algorithm = JetAlgorithm.GenKt,
+                                                          R = 0.4))==ClusterSequence{PseudoJet}
 
         # No algorithm or power will throw
-        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1])) == ClusterSequence{PseudoJet}
+        @test_throws ArgumentError typeof(jet_reconstruct(inputs[1]))==ClusterSequence{PseudoJet}
     end
 end
