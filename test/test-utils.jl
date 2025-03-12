3e3e3fbc9ba575b7600f9892570bf69cdfc25366
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2025-03-12 13:33:25 +0100

    Remove debugging line

--- test/test-utils.jl
+++ test/test-utils.jl
@@ -8,7 +8,6 @@ const string_target = "Hello, JetReconstruction!"
 @testset "File streaming" begin
     for fname in ["file.txt", "file.txt.gz", "file.txt.zst"]
         fname = joinpath(@__DIR__, "data", fname)
-        println("Testing $fname")
         string = readline(JetReconstruction.open_with_stream(fname))
         @test string == string_target
     end
