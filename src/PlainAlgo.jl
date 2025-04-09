bcdcc6c4baa14e37c5b95a98069aeeeaeb9d5cc2
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2025-04-09 17:23:44 +0100

    Fix conversion to internal jet types
    Need to add the correct history index

--- src/PlainAlgo.jl
+++ src/PlainAlgo.jl
@@ -244,7 +244,7 @@ function plain_jet_reconstruct(particles::AbstractVector{T}; p::Union{Real, Noth
         for i in eachindex(particles)
             push!(recombination_particles,
                   PseudoJet(px(particles[i]), py(particles[i]), pz(particles[i]),
-                            energy(particles[i])))
+                            energy(particles[i]), i))
         end
     end
 
