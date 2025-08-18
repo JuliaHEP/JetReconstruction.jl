db055a87c1b2f807d6e15c5bfe05809c881479dd
Matt LeBlanc <matt.leblanc@cern.ch>
2025-08-18 11:29:06 -0400

    Cleanup after failed squash ...

--- src/EEJet.jl
+++ src/EEJet.jl
@@ -22,7 +22,7 @@ struct EEJet <: FourMomentum
     _cluster_hist_index::Int
 end
 
-g"""
+"""
     EEJet(px::Real, py::Real, pz::Real, E::Real, cluster_hist_index::Int)
 
 Constructs an `EEJet` object from the given momentum components, energy, and
