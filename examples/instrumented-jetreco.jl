cc85d5020b8a03ff6ed3bd38007dfed8d16edcfb
Graeme Andrew Stewart <graeme.andrew.stewart@desy.de>
2026-02-17 15:36:57 +0100

    Formatting

--- examples/instrumented-jetreco.jl
+++ examples/instrumented-jetreco.jl
@@ -249,7 +249,8 @@ Generate a "pretty printed" string from a StatsBase statistics object,
 which is basically use @sprintf to limit the precision
 """
 function pprint_trial_stats(sstats)
-    " - average time per event " * @sprintf("%.2f", sstats.mean) * " ± " * @sprintf("%.2f", sstats.sd) * " μs\n" *
+    " - average time per event " * @sprintf("%.2f", sstats.mean) * " ± " *
+    @sprintf("%.2f", sstats.sd) * " μs\n" *
     " - lowest time per event " * @sprintf("%.2f", sstats.min) * " μs"
 end
 
