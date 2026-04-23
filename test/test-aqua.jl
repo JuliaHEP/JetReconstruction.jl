6c35b36c9bfecbd08eab0c2a2a72c3b72cecb502
Graeme Andrew Stewart <graeme.andrew.stewart@desy.de>
2026-04-23 08:55:00 +0200

    Fix to allow Aqua test to run standalone

--- test/test-aqua.jl
+++ test/test-aqua.jl
@@ -1,8 +1,9 @@
 # Use the Julia Aqua tests
 # Aqua.jl: Auto QUality Assurance for Julia packages
+include("common.jl")
+
 using Aqua
 
-include("common.jl")
 @testset "Aqua.jl" begin
     Aqua.test_all(JetReconstruction)
 end
