458680dee66a698eb982ea0f523d27deabe570cb
Graeme Andrew Stewart <graeme.andrew.stewart@desy.de>
2026-06-12 12:53:55 +0200

    Update to EDM4hep v1.0
    Include support fo the 1.0 release of EDM4hep.jl.
    
    Conversion to JetReco types is now supported for both MCParticle
    and ReconstructedParticle types.
    
    In the example file SimpleRecoEDM4hep.jl take the input file from the
    EDM4hep package, so there is no longer a dangling dependence on a
    local file.
    
    A tiny "dump" script, QuickLookEDM4hep.jl is added for debugging.

---
+++ examples/EDM4hep/QuickLookEDM4hep.jl
@@ -0,0 +1,16 @@
+# A very simple interrogator to quickly get the contents of an EDM4hep ROOT
+# file - useful for debugging and developing!
+using EDM4hep
+using EDM4hep.RootIO
+using JetReconstruction
+
+function main()
+    input_file = ARGS[1]
+    reader = RootIO.Reader(input_file)
+    events = RootIO.get(reader, "events")
+    evt = events[1]
+    # See everything!
+    show(IOContext(stdout, :limit => false, :compact => false, :displaysize => (1000, 200)), reader)
+end
+
+main()
