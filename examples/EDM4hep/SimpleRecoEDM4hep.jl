f6048aed782ba459bd288a84c5e02f5b56baaa8a
Graeme A Stewart <graeme.andrew.stewart@desy.de>
2026-06-17 08:24:45 +0200

    Update to EDM4hep v1.0 (#249)
    Include support for the 1.0 release of EDM4hep.jl.
    
    Conversion to JetReco types is now supported for both MCParticle
    and ReconstructedParticle types.
    
    In the example file SimpleRecoEDM4hep.jl take the input file from the
    EDM4hep package, so there is no longer a dangling dependence on a
    local file.
    
    A tiny "dump" script, QuickLookEDM4hep.jl is added for debugging.

--- examples/EDM4hep/SimpleRecoEDM4hep.jl
+++ examples/EDM4hep/SimpleRecoEDM4hep.jl
@@ -3,24 +3,24 @@ using EDM4hep
 using EDM4hep.RootIO
 using JetReconstruction
 
-input_file = joinpath("/", "Users", "graemes", "code", "EDM4hepJets", "data",
-                      "events_196755633.root")
+input_file = joinpath(pkgdir(EDM4hep), "examples", "ttbar_edm4hep_digi.root")
 reader = RootIO.Reader(input_file)
+
 events = RootIO.get(reader, "events");
 
 evt = events[1];
 
-recps = RootIO.get(reader, evt, "ReconstructedParticles")
+recps = RootIO.get(reader, evt, "MCParticle")
 
 # Reconstruct and print the jets
-cs = jet_reconstruct(recps; algorithm = JetAlgorithm.Durham)
-dijets = exclusive_jets(cs, EEJet; njets = 2)
-for jet in dijets
+cs = jet_reconstruct(recps; algorithm = JetAlgorithm.AntiKt)
+jets = inclusive_jets(cs, PseudoJet; ptmin = 5.0)
+for jet in jets
     println(jet)
 end
 
 # Get constituents
-for (i, jet) in enumerate(dijets)
+for (i, jet) in enumerate(jets)
     my_constituent_indexes = constituent_indexes(jet, cs)
     println("Jet $i constituents: $my_constituent_indexes")
 end
--- ext/EDM4hepJets.jl
+++ ext/EDM4hepJets.jl
@@ -5,55 +5,59 @@ module EDM4hepJets
 using JetReconstruction
 using EDM4hep
 
-"""
-    JetReconstruction.px(recoparticle::ReconstructedParticle)
-
-Return the x component of the momentum of a ReconstructedParticle.
-"""
-JetReconstruction.px(recoparticle::ReconstructedParticle) = recoparticle.momentum.x
+# Make a union so that we can consider MC and Reco particles to
+# be the same types for the following methods
+const EDM4hepParticle = Union{MCParticle, ReconstructedParticle}
 
 """
-    JetReconstruction.py(recoparticle::ReconstructedParticle)
+    JetReconstruction.px(particle::EDM4hepParticle)
 
-Return the y component of the momentum of a ReconstructedParticle.
+Return the x component of the momentum of a EDM4hepParticle.
 """
-JetReconstruction.py(recoparticle::ReconstructedParticle) = recoparticle.momentum.y
-
-"""
-    JetReconstruction.pz(recoparticle::ReconstructedParticle)
-
-Return the z component of the momentum of a ReconstructedParticle.
-"""
-JetReconstruction.pz(recoparticle::ReconstructedParticle) = recoparticle.momentum.z
+JetReconstruction.px(particle::EDM4hepParticle) = particle.momentum.x
 
 """
-    JetReconstruction.energy(recoparticle::ReconstructedParticle)
+    JetReconstruction.py(particle::EDM4hepParticle)
 
-Return the energy component of a ReconstructedParticle's four vector.
+Return the y component of the momentum of a EDM4hepParticle.
 """
-JetReconstruction.energy(recoparticle::ReconstructedParticle) = recoparticle.energy
+JetReconstruction.py(particle::EDM4hepParticle) = particle.momentum.y
 
 """
-    JetReconstruction.EEJet(recoparticle::ReconstructedParticle)
+    JetReconstruction.pz(particle::EDM4hepParticle)
 
-Construct an EEJet from a ReconstructedParticle.
+Return the z component of the momentum of a EDM4hepParticle.
 """
-function JetReconstruction.EEJet(recoparticle::ReconstructedParticle;
+JetReconstruction.pz(particle::EDM4hepParticle) = particle.momentum.z
+
+"""
+    JetReconstruction.energy(particle::EDM4hepParticle)
+
+Return the energy component of a EDM4hepParticle's four vector.
+"""
+JetReconstruction.energy(particle::EDM4hepParticle) = particle.energy
+
+"""
+    JetReconstruction.EEJet(particle::EDM4hepParticle)
+
+Construct an EEJet from a EDM4hepParticle.
+"""
+function JetReconstruction.EEJet(particle::EDM4hepParticle;
                                  cluster_hist_index::Int = 0)
-    EEJet(JetReconstruction.px(recoparticle), JetReconstruction.py(recoparticle),
-          JetReconstruction.pz(recoparticle), JetReconstruction.energy(recoparticle);
+    EEJet(JetReconstruction.px(particle), JetReconstruction.py(particle),
+          JetReconstruction.pz(particle), JetReconstruction.energy(particle);
           cluster_hist_index)
 end
 
 """
-    JetReconstruction.PseudoJet(recoparticle::ReconstructedParticle)
+    JetReconstruction.PseudoJet(particle::EDM4hepParticle)
 
-Construct an PseudoJet from a ReconstructedParticle.
+Construct an PseudoJet from a EDM4hepParticle.
 """
-function JetReconstruction.PseudoJet(recoparticle::ReconstructedParticle;
+function JetReconstruction.PseudoJet(particle::EDM4hepParticle;
                                      cluster_hist_index::Int = 0)
-    PseudoJet(JetReconstruction.px(recoparticle), JetReconstruction.py(recoparticle),
-              JetReconstruction.pz(recoparticle), JetReconstruction.energy(recoparticle);
+    PseudoJet(JetReconstruction.px(particle), JetReconstruction.py(particle),
+              JetReconstruction.pz(particle), JetReconstruction.energy(particle);
               cluster_hist_index)
 end
 
