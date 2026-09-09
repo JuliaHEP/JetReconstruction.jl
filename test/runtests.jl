7add4698401d230cfe143143579431b5533d38aa
HarshitNagpal29 <harshitnagpal161@gmail.com>
2026-09-10 01:05:20 +0530

    Remove deleted workspace test from test runner

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,7 +21,6 @@ function main()
     include("test-pp-reconstruction.jl")
 
     include("test-n2plain-kernel.jl")
-    include("test-workspace-equivalence.jl")
 
     include("test-ee-reconstruction.jl")
 
