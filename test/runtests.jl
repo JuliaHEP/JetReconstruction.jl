ab438453d7d80ac97fac8619a822c954954674eb
Harshit Nagpal <harshitnagpal161@gmail.com>
2026-09-10 01:35:04 +0530

    Apply suggestion from @m-fila
    Co-authored-by: Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,9 +19,8 @@ function main()
 
     # New test structure, factorised tests for pp and e+e-
     include("test-pp-reconstruction.jl")
-
+    include("test-n2tiled-kernel.jl")
     include("test-n2plain-kernel.jl")
-
     include("test-ee-reconstruction.jl")
 
     # Valencia algorithm tests
